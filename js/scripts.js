243e98f0731306c9e669878b9bfa2998d3185b22
Gabriel Ayala <gabeayala100@gmail.com>
2021-06-09 11:53:49 -0700

    Add a remove class method to each button method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
   $("button#green").click(function() {
+    $("body").removeClass();
     $("body").addClass("green-background");
-});
+    });
 
-$("button#yellow").click(function() {
-  $("body").addClass("yellow-background");
-});
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
 
-$("button#red").click(function() {
-  $("body").addClass("red-background");
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
 });
